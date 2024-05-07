f5bc2e133c48f39af33ea2533f34ec024405fcad
youassassin <youassassin>
2024-05-06 21:45:18 -0400

    added sorting foreign master table

--- monster-trainer-database/src/app/dragon-warrior-monster/dwm-monster/dwm-master-monster/dwm-master-monster.component.ts
+++ monster-trainer-database/src/app/dragon-warrior-monster/dwm-monster/dwm-master-monster/dwm-master-monster.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DragonWarriorMonsterService } from '../../dragon-warrior-monster.service';
 import { MasterMonster } from '../../model/dwm-master-monster.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -10,12 +10,18 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class DwmMasterMonsterComponent implements OnInit {
 
+  @ViewChild('tMonImg') monsterImg!: ElementRef;
+  @ViewChild('tLvlImg') lvlImg!: ElementRef;
   form: FormGroup;
   previousStates: any;
   masters: MasterMonster[] = [];
   table: MasterMonster[] = [];
   skills: string[] = [];
   monsters: string[] = [];
+
+  readonly UP_PATH = '../../../../assets/icons/sortable-up.png';
+  readonly EVEN_PATH = '../../../../assets/icons/sortable-even.png';
+  readonly DOWN_PATH = '../../../../assets/icons/sortable-down.png';
   constructor(private dwmService: DragonWarriorMonsterService) {
     this.form = new FormBuilder().group({
       'monOne': '',
@@ -57,7 +63,6 @@ export class DwmMasterMonsterComponent implements OnInit {
     }
     this.previousStates[id] = this.form.get(id)?.value;
     this.updateTableLvl();
-    console.log(event);
   }
   calculateAndSetTotalValue() {
     this.form.get('monTot')?.setValue(
@@ -136,4 +141,50 @@ export class DwmMasterMonsterComponent implements OnInit {
       this.skills.sort();
     }
   }
+  sort(event: any) {
+    let id = event.target.id;
+    if (id === 'tLvl' || id === 'tLvlImg') {
+      let imgDirection = this.lvlImg.nativeElement.src;
+      if (imgDirection.endsWith('even.png') || imgDirection.endsWith('up.png')) {
+        this.lvlImg.nativeElement.src = this.DOWN_PATH;
+        this.monsterImg.nativeElement.src = this.EVEN_PATH;
+        this.sortList(true, false);
+      } else {
+        this.lvlImg.nativeElement.src = this.UP_PATH;
+        this.monsterImg.nativeElement.src = this.EVEN_PATH;
+        this.sortList(false, false);
+      }
+    } else if (id === 'tMon' || id === 'tMonImg') {
+      let imgDirection = this.monsterImg.nativeElement.src;
+      if (imgDirection.endsWith('even.png') || imgDirection.endsWith('up.png')) {
+        this.monsterImg.nativeElement.src = this.DOWN_PATH;
+        this.lvlImg.nativeElement.src = this.EVEN_PATH;
+        this.sortList(true, true);
+      } else {
+        this.monsterImg.nativeElement.src = this.UP_PATH;
+        this.lvlImg.nativeElement.src = this.EVEN_PATH;
+        this.sortList(false, true);
+      }
+    }
+  }
+
+  sortList(isUp: boolean, isMonster: boolean) {
+    if (isUp && isMonster)
+      this.table.sort((a, b) => a.monster < b.monster ? -1 : (a.monster > b.monster ? 1 : 0));
+    else if (!isUp && isMonster)
+      this.table.sort((a, b) => b.monster < a.monster ? -1 : (b.monster > a.monster ? 1 : 0));
+    else if (isUp && !isMonster)
+      this.table.sort((a, b) => a.lowerTeamLvl - b.lowerTeamLvl);
+    else
+      this.table.sort((a, b) => b.lowerTeamLvl - a.lowerTeamLvl);
+  }
+  // swapImages(clicked: any, other: any) {
+  //   if (clicked.endsWith('even.png') || clicked.endsWith('up.png')) {
+  //     clicked = this.DOWN_PATH;
+  //     other = this.EVEN_PATH;
+  //   } else {
+  //     clicked = this.UP_PATH;
+  //     other = this.EVEN_PATH;
+  //   }
+  // }
 }
